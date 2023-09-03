fed52d2b7e6ac560efff715fb8a3b40d7f6eea4b
Navin <rainavin@gmail.com>
2023-09-03 22:52:26 +0800

    initial

--- employee-service/src/main/java/com/navexmp/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/navexmp/employeeservice/EmployeeServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class EmployeeServiceApplication {
-
+//main method
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
